956d6a4153a33dcd412f020bfc1c479c594bd47f
Artem Skripka <therewasadrizzle@gmail.com>
2016-11-29 12:48:37 +0700

    add root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :tasks, only: [:index, :new, :create, :edit, :update]
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+  root 'tasks#index'
 end
