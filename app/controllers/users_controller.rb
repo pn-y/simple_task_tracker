d00f8e5d306444ac9048d6476d5d9502e0fe68b1
Artem Skripka <therewasadrizzle@gmail.com>
2016-11-29 10:10:12 +0700

    add users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+  def new
+    return if check_already_singed_in
+    @user = User.new
+  end
+
+  def create
+    return if check_already_singed_in
+    @user = User.new(user_params)
+
+    if @user.save
+      sign_in(@user)
+      redirect_to tasks_url, notice: t('user.flash.create')
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
